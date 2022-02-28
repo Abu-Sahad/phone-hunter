38bc2df79c9043cdfd2bdce1f62fd77b19183ad9
Abu-Sahad <abusahad603@gmail.com>
2022-02-28 23:58:26 +0600

    details half complete

--- js/phone.js
+++ js/phone.js
@@ -17,9 +17,35 @@ const displaySearchResult = (phones) => {
         <div class="card-body">
                <h2 class="phone-name">${phone.phone_name}</h2>
                <h3 class="phone-brand">${phone.brand}</h3>
+               <button onclick="phoneDetails('${phone.slug}')" id="cal-button" class="calculator-button mx-auto mt-3 mb-3">Details</button>
         </div>
+        
     </div>`
         Totalphone.appendChild(div)
 
     });
+}
+const phoneDetails = (id) => {
+    const url = ` https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => setDetails(data.data))
+}
+
+const setDetails = (info) => {
+    const phoneDetail = document.getElementById('phone-details').innerHTML = `
+    <div>
+    <img class="card-img-top" src="${info.image}" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+            the
+            card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>`;
+
+
+
+
 }
