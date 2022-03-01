c3bfe24803384525a954c49084320f4682d91528
Abu-Sahad <abusahad603@gmail.com>
2022-03-01 16:10:30 +0600

    sensors and others half done

--- js/phone.js
+++ js/phone.js
@@ -35,8 +35,8 @@ const displaySearchResult = (phones) => {
                 <img src="${phone.image}" class="card-img-top w-75  mt-3" alt="...">
             </div>
             <div class="card-body text-center">
+            <p class="phone-name"><span class="fw-bolder">Model: </span>${phone.phone_name}<p>
                <h4 class="phone-brand">Brand: ${phone.brand}</h4>
-               <p class="phone-name"><span class="fw-bolder">Model: </span>${phone.phone_name}<p>
                <button onclick="phoneDetails('${phone.slug}')" id="cal-button" class="calculator-button mx-auto mt-3 mb-3">Details</button>
             </div>
         
@@ -61,13 +61,25 @@ const setDetails = (info) => {
         </div>
         <div class="card-body">
             <p class="phone-details"><span class="fw-bolder">Phone Name: </span> ${info.name}</p>
-            <p class="phone-details"><span class="fw-bolder">Release-Date: </span>${info.releaseDate}</p>
+            <p class="phone-details"><span class="fw-bolder">Release-Date: </span>${info.releaseDate ? info.releaseDate : 'relese date comming soon'}</p>
             <h5 class="other-details">Main features:</h5>
             <p class="phone-details"><span class="fw-bolder">Chipset: </span>${info.mainFeatures.chipSet}</p>
             <p class="phone-details"><span class="fw-bolder">display-Size: </span>${info.mainFeatures.displaySize}</p>
             <p class="phone-details"><span class="fw-bolder">Memory: </span>${info.mainFeatures.memory}</p>
-            <p class="phone-details"><span class="fw-bolder">Release-Date: </span>${info.releaseDate}</p>
+            
+            <p class="phone-sensors"><span class="fw-bolder">Sensor: </span>${info.mainFeatures.sensors}</p>
+            
+            <h4 class="text-info"> Others Information</h4>
+            <p class="phone-others"><span class="fw-bolder">WLAN: </span>${info.others.WLAN}</p>
+            <p class="phone-others"><span class="fw-bolder">Bluetooth: </span>${info.others.Bluetooth}</p>
+            <p class="phone-others"><span class="fw-bolder">GPS: </span>${info.others.GPS}</p>
+            <p class="phone-others"><span class="fw-bolder">NFC: </span>${info.others.NFC}</p>
+            <p class="phone-others"><span class="fw-bolder">Radio: </span>${info.others.Radio}</p>
+            <p class="phone-others"><span class="fw-bolder">USB: </span>${info.others.USB}</p>
+            <p class="phone-others"><span class="fw-bolder">WLAN: </span>${info.others.WLAN}</p>
+            
             <button onclick="phoneMoreInfo('')" id="cal-button" class="calculator-button mx-auto mt-3 mb-3">More Info</button>
+
         
         </div>
     </div>`;
