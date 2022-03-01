b6d487db9483db113b7bb2fdbcbb0d53d8969f50
Abu-Sahad <abusahad603@gmail.com>
2022-03-01 15:31:39 +0600

    more features add

--- js/phone.js
+++ js/phone.js
@@ -1,28 +1,47 @@
 const allPhone = () => {
-    const searchText = document.getElementById('phoneSearch').value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    fetch(url)
-        .then((res) => res.json())
-        .then((data) => displaySearchResult(data.data))
+    const searchField = document.getElementById('phoneSearch');
+    const searchText = searchField.value
+    //clear data
+    searchField.value = '';
+    if (searchText == '') {
+        //please write someting to display 
+    }
+    else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => displaySearchResult(data.data))
+    }
+
 
 
 }
 const displaySearchResult = (phones) => {
-    const Totalphone = document.getElementById('total-phone')
+    const totalPhone = document.getElementById('total-phone')
+    //Load data clear
+    totalPhone.textContent = '';
+
+    if (phones.length == 0) {
+        //show no result found
+    }
+
     phones.slice(0, 20).forEach(phone => {
 
         const div = document.createElement('div');
         div.classList.add('col');
-        div.innerHTML = `<div class="card h-100">
-        <img src="${phone.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-               <h2 class="phone-name">${phone.phone_name}</h2>
-               <h3 class="phone-brand">${phone.brand}</h3>
+        div.innerHTML = `
+        <div class="card h-100">
+            <div class="img-info ">
+                <img src="${phone.image}" class="card-img-top w-75  mt-3" alt="...">
+            </div>
+            <div class="card-body text-center">
+               <h4 class="phone-brand">Brand: ${phone.brand}</h4>
+               <p class="phone-name"><span class="fw-bolder">Model: </span>${phone.phone_name}<p>
                <button onclick="phoneDetails('${phone.slug}')" id="cal-button" class="calculator-button mx-auto mt-3 mb-3">Details</button>
-        </div>
+            </div>
         
-    </div>`
-        Totalphone.appendChild(div)
+        </div>`
+        totalPhone.appendChild(div)
 
     });
 }
@@ -34,20 +53,20 @@ const phoneDetails = (id) => {
 }
 
 const setDetails = (info) => {
-    console.log(info)
+    textContent = '';
     document.getElementById("phone-details").innerHTML = `
-    <div class="card h-100">
-       <div class="details-img-info ">
-           <img class="card-img-top w-50 h-25 mt-3" src="${info.image}" alt="Card image cap">
+    <div class="card h-100 ">
+       <div class="img-info ">
+           <img class="card-img-top w-50 img-fluid mx-auto mt-3" src="${info.image}" alt="Card image cap">
         </div>
         <div class="card-body">
-           <p class="">Release-Date:${info.releaseDate}</p>
-           <h5 class="other-details">Main features:<br>
-              Chipset:${info.mainFeatures.chipSet}<br>
-              display-Size:${info.mainFeatures.displaySize}<br>
-              Memory:${info.mainFeatures.memory}
-            </h5>
-        
+            <p class="phone-details"><span class="fw-bolder">Phone Name: </span> ${info.name}</p>
+            <p class="phone-details"><span class="fw-bolder">Release-Date: </span>${info.releaseDate}</p>
+            <h5 class="other-details">Main features:</h5>
+            <p class="phone-details"><span class="fw-bolder">Chipset: </span>${info.mainFeatures.chipSet}</p>
+            <p class="phone-details"><span class="fw-bolder">display-Size: </span>${info.mainFeatures.displaySize}</p>
+            <p class="phone-details"><span class="fw-bolder">Memory: </span>${info.mainFeatures.memory}</p>
+            <p class="phone-details"><span class="fw-bolder">Release-Date: </span>${info.releaseDate}</p>
             <button onclick="phoneMoreInfo('')" id="cal-button" class="calculator-button mx-auto mt-3 mb-3">More Info</button>
         
         </div>
